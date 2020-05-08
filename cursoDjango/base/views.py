c6f7bd2849e2bc910d0a2a69706671fd2e83d80a
Ilito Torquato <ilitotor@gmail.com>
2020-05-08 10:56:57 -0300

    fixing flake8

--- cursoDjango/base/views.py
+++ cursoDjango/base/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
     return HttpResponse('Olá Django)')
-
